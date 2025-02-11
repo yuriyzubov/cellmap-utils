9dfd81fb3cda36a1fb732a2fc26c7deb775ec147
zubovy <zubov452@gmail.com>
2025-02-11 15:13:51 -0500

    Import methods into __init__.py, to make them visible when using pip package

--- src/cellmap_utils/__init__.py
+++ src/cellmap_utils/__init__.py
@@ -1,7 +1,7 @@
 #from cellmap_utils._version import version
 
-__version__ = "0.0.1"
+__version__ = "0.0.11"
 
-from .zarr import get_multiscale_metadata, access_parent, separate_store_path
+from .zarr import get_multiscale_metadata, access_parent, separate_store_path, insert_omero_metadata, ome_ngff_only, repair_zarr_branch 
 
-#__all__ = ["get_multiscale_metadata", "access_parent", "separate_store_path"]
+__all__ = ["get_multiscale_metadata", "access_parent", "separate_store_path", "separate_store_path", "insert_omero_metadata", "ome_ngff_only", "repair_zarr_branch" ]
--- src/cellmap_utils/zarr/__init__.py
+++ src/cellmap_utils/zarr/__init__.py
@@ -1,3 +1,3 @@
-from .metadata import get_multiscale_metadata, get_single_scale_metadata
+from .metadata import get_multiscale_metadata, get_single_scale_metadata, insert_omero_metadata, ome_ngff_only
 from .store import separate_store_path
-from .node import access_parent
+from .node import access_parent, repair_zarr_branch

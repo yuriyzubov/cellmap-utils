0aba4c5549dfb54937e6d117f940a1effe35f9b4
zubovy <zubov452@gmail.com>
2025-02-14 13:12:01 -0500

    Broken: airtable upserts

---
+++ src/cellmap_utils/airtable/image.py
@@ -0,0 +1,3 @@
+from typing import Tuple
+from pyairtable import api
+from pyairtable.formulas import match
---
+++ src/cellmap_utils/airtable/pydantic_models.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import Dict, Any
+import json
+
+
+def generate_pydantic_model(data: Dict[str, Any], model_name: str ) -> type[BaseModel]:
+    """
+    Generates a Pydantic model class dynamically from a dictionary.
+
+    Args:
+        data: A dictionary representing the structure of the model.
+        model_name: The name of the generated Pydantic model class.
+
+    Returns:
+        A Pydantic model class.
+    """
+
+    fields = {}
+    for key, value in data.items():
+        fields[key] = (type(value), ...)
+
+    model_class = type(model_name, (BaseModel,), fields)
+    return model_class
+
+class AirImageModel(BaseModel):

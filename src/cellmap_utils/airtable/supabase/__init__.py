e88508691fb8cb0f45b2487ca09d40092624e848
zubovy <zubov452@gmail.com>
2025-03-25 16:01:24 -0400

    Add methods for fetching dataset and image info from airtable and combining it into supabase table records

--- src/cellmap_utils/airtable/supabase/__init__.py
+++ src/cellmap_utils/airtable/supabase/__init__.py
@@ -1,5 +1,4 @@
-from .upsert import upsert_image, upsert_record_scene_tables
-from .supabase import (get_image_record,
+from air_to_supabase import (get_image_record,
                              get_img_acq_record,
                              get_sample_record,
                              get_dataset_record,

d08ee8c8c969957b92d5216cb3c24959911bd279
zubovy <zubov452@gmail.com>
2025-04-09 14:41:13 -0400

    Modify methods that retrieve necessary info to construct supabase database records

--- src/cellmap_utils/airtable/supabase/__init__.py
+++ src/cellmap_utils/airtable/supabase/__init__.py
@@ -1,7 +1,5 @@
-from air_to_supabase import (
-    get_image_record,
-    get_img_acq_record,
-    get_sample_record,
-    get_dataset_record,
-    get_dataset_full,
-)
+from .air_to_supabase import (get_image_record,
+                             get_img_acq_record,
+                             get_sample_record,
+                             get_dataset_record,
+                             get_dataset_full)
